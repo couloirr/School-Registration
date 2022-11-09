7aa0f89f3f9762c5f0cec5b4d44abf013b894a3b
Adam Seery <102638662+couloirr@users.noreply.github.com>
2022-11-09 14:56:39 -0700

    basic app template setup front and back

---
+++ server/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path'); // NEW
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const DIST_DIR = path.join(__dirname, '../dist'); // NEW
+const HTML_FILE = path.join(DIST_DIR, 'index.html'); // NEW
+const mockResponse = {
+  foo: 'bar',
+  bar: 'foo',
+};
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use(express.urlencoded({ extended: true }));
+app.get('/api', (req, res) => {
+  res.send(mockResponse);
+});
+app.use('/assets', express.static(path.join(__dirname, '../src/assets')));
+app.get('*', (req, res) => {
+  return res.status(200).sendFile(HTML_FILE);
+});
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+app.listen(port, function () {
+  console.log('App listening on port: ' + port);
+});
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  mode: process.env.NODE_ENV,
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html'),
+    }),
+  ],
+  devServer: {
+    historyApiFallback: true,
+    port: 8080,
+    static: {
+      publicPath: '/',
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
